76bcd8a38af0222c3d4138efdcc107aafda8fec1
hayz999 <hlzulkoski@gmail.com>
2018-12-09 10:52:13 -0700

    Heroku db connection

--- booksList/config/database.php
+++ booksList/config/database.php
@@ -1,4 +1,6 @@
 <?php
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+$DB_CONNECTION = parse_url(getenv("DB_CONNECTION"));
 
 return [
 
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', $DB_CONNECTION),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +60,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
